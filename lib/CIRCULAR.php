c369a7985f1e0e56c266356a049fa9727aa18f0b
Danny De Novi <70696078+dannydenovi@users.noreply.github.com>
2024-09-28 14:52:12 +0200

    Update CIRCULAR.php

--- lib/CIRCULAR.php
+++ lib/CIRCULAR.php
@@ -4,22 +4,24 @@
  |
  | File_Name       : CIRCULAR.php
  | Author          : Eric D. Wade ;., (nandesu@gmail.com)
+ | Collaborators   : Danny De Novi (dannydenovi29@gmail.com)
  | Date            : 
- | Description     : This is the PHP port of circular protocal SDK
+ | Description     : This is the PHP port of circular protocol SDK
  *
  *
  *
- |  Modified Date       :
- |  Last Modified By    :
- |  Modification Notes  :
+ |  Modified Date       : 28/09/2024
+ |  Last Modified By    : Danny De Novi
+ |  Modification Notes  : Fuxed send transaction and wallet registration methods
  *
  *
  *---------------------------------------------------------------------------*/
-
 /*---------------------------------------------------------------------------
  | INCLUDES
  *---------------------------------------------------------------------------*/
 use Elliptic\EC;
+use Elliptic\Utils;
+
 
 /*---------------------------------------------------------------------------
  | VARIABLES
@@ -55,7 +57,7 @@ private function fetch($url, $data)
  | Returns      : result object
  | Description  : Mimic the JS Fetch Command to send a JSON payload.
  *
- */
+ */ 
 {
 $options = array(
      'http' => array(
@@ -64,8 +66,9 @@ $options = array(
                          'content' => json_encode($data)
                     )
     );
+
     $context  = stream_context_create($options);
-    $result = file_get_contents(NAG_URL . 'Circular_CallContract_', false, $context);
+    $result = file_get_contents($url, false, $context);
 
     if ($result === FALSE) { throw new Exception('Network response was not ok'); }
 
@@ -149,7 +152,7 @@ private function hexFix($word)
  *
  */
 {
-    return preg_replace('/^0x/', '', $word);
+    return preg_replace('/^0x|\\\\|\n|\r/', '', $word);
 }
 /*_______________________________________________________________________*/
 
@@ -191,6 +194,8 @@ private function verifySignature($publicKey, $message, $signature)
     return $key->verify($msgHash, $signature, 'hex');
 }
 /*_______________________________________________________________________*/
+
+
 
 /*_______________________________________________________________________*/
 private function getPublicKey($privateKey) 
@@ -360,6 +365,30 @@ public function getWalletBalance($blockchain, $address, $asset)
 }
 /*_______________________________________________________________________*/
 
+
+/*_______________________________________________________________________*/
+public function getWalletNonce($blockchain, $address)
+/*
+ | Variables    : string, string, string
+ | Returns      : JSON
+ | Description  : Retrieves the balance of a specified asset in a Wallet
+ *                Blockchain: Blockchain where the wallet is registered
+ *                Address: Wallet address
+ *                Asset: Asset Name (example 'CIRX')
+ *
+ */
+{
+    $blockchain = $this->hexFix($blockchain);
+    $address    = $this->hexFix($address);
+    $data       = array(
+                        "Blockchain" => $blockchain,
+                        "Address"    => $address,
+                        "Version"    => $this->version
+                  );
+    return $this->fetch($this->NAG_URL . 'Circular_GetWalletNonce_', $data);
+}
+/*_______________________________________________________________________*/
+
 /*_______________________________________________________________________*/
 public function getLatestTransactions($blockchain, $address)
 /*
@@ -381,7 +410,7 @@ public function getLatestTransactions($blockchain, $address)
 /*_______________________________________________________________________*/
 
 /*_______________________________________________________________________*/
-public function registerWallet($blockchain, $privateKey) 
+public function registerWallet($blockchain, $publicKey) 
 /*
  | Variables    : string, string
  | Returns      : string
@@ -395,8 +424,7 @@ public function registerWallet($blockchain, $privateKey)
  */
 {
     $blockchain = $this->hexFix($blockchain);
-    $privateKey = $this->hexFix($privateKey);
-    $publicKey  = $this->getPublicKey($privateKey);
+    $publicKey = $this->hexFix($publicKey);
     $from       = hash('sha256', $publicKey);
     $to         = $from;
     $nonce      = '0';
@@ -404,14 +432,13 @@ public function registerWallet($blockchain, $privateKey)
     $payloadObj = array(
                         "Action"    => "CP_REGISTERWALLET",
                         "PublicKey" => $publicKey,
-                        "Version"   => $this->version
                   );
     $jsonstr    = json_encode($payloadObj);
     $payload    = $this->stringToHex($jsonstr);
     $timestamp  = $this->getFormattedTimestamp();
-    $id         = hash('sha256', $from . $to . $payload . $nonce . $timestamp);
-    $signature  = $this->signMessage($id, $privateKey);
-    $result     = $this->sendTransaction($id, $from, $to, $timestamp, $type, $payload, $nonce, $publicKey, $signature, $blockchain);
+    $id         = hash('sha256', $blockchain . $from . $to . $payload . $nonce . $timestamp);
+    $signature  = "";
+    $result     = $this->sendTransaction($id, $from, $to, $timestamp, $type, $payload, $nonce, $signature, $blockchain);
     return $result;
 }
 /*_______________________________________________________________________*/
@@ -669,12 +696,39 @@ public function getTransactionByDate($blockchain, $address, $startDate, $endDate
 
 public function sendTransaction($id, $from, $to, $timestamp, $type, $payload, $nonce, $signature, $blockchain) 
 {
-    $from = $this->hexFix($from);
-    $to = $this->hexFix($to);
-    $id = $this->hexFix($id);
-    $payload = $this->hexFix($payload);
-    $signature = $this->hexFix($signature);
+    $from       = $this->hexFix($from);
+    $to         = $this->hexFix($to);
+    $id         = $this->hexFix($id);
+    $payload    = $this->hexFix($payload);
+    $signature  = $this->hexFix($signature);
     $blockchain = $this->hexFix($blockchain);
+    $data = [
+        "ID"         => $id,
+        "From"       => $from,
+        "To"         => $to,
+        "Timestamp"  => $timestamp,
+        "Payload"    => strval($payload),
+        "Nonce"      => strval($nonce),
+        "Signature"  => $signature,
+        "Blockchain" => $blockchain,
+        "Type"       => $type,
+        "Version"    => $this->version
+    ];
+
+    return $this->fetch($this->NAG_URL . 'Circular_AddTransaction_', $data);
+}
+
+public function sendTransactionWithPK($from, $senderPK, $to, $type, $payload, $blockchain){
+
+    $from       = $this->hexFix($from);
+    $to         = $this->hexFix($to);
+    $payload    = $this->stringToHex(json_encode($payload));
+    $blockchain = $this->hexFix($blockchain);
+    $nonce      = intval($this->getWalletNonce($blockchain, $from)->Response->Nonce) + 1;
+    $timestamp  = $this->getFormattedTimestamp();
+    $id         = hash("sha256", $blockchain . $from . $to . $payload . strval($nonce) . $timestamp, false);
+    $signature  = $this->signMessage($id, $senderPK);
+
     $data = [
         "ID"         => $id,
         "From"       => $from,
@@ -707,10 +761,29 @@ public function getTransactionOutcome($blockchain, $txID, $timeoutSec)
 	    if ($transactionData && $transactionData['Result'] === 200 && 
 		    $transactionData['Response'] !== 'Transaction Not Found' && 
 		    $transactionData['Response']['Status'] !== 'Pending') 
-	    { return $transactionData['Response']; // Return the transaction data if found and not pending }
+	    { return $transactionData['Response']; // Return the transaction data if found and not pending
+	    }
 
             sleep($intervalSec); // Wait for the interval before checking again
         }
+    }
+
+
+    public function keysFromSeedPhrase($seedphrase)
+    {   
+        $seed = hash('sha256', $seedphrase, false); 
+        $ec = new EC('secp256k1');
+        $keyPair = $ec->keyFromPrivate($seed);
+
+        $privateKey = $keyPair->getPrivate('hex');
+        $publicKey = $keyPair->getPublic('hex');
+        $walletAddress = hash("sha256", $publicKey, false);
+
+        return [
+            'privateKey'    => $privateKey,
+            'publicKey'     => $publicKey,
+            'walletAddress' => $walletAddress
+        ];
     }
 
 } // end of class: 
